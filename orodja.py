4f2febe71b1c5ecdf0f3b5aafc7f08b3a58fce9c
metodj <jazbec.metod@gmail.com>
2017-01-31 16:24:03 +0100

    Add files via upload

---
+++ orodja.py
@@ -0,0 +1,51 @@
+import csv
+import os
+import requests
+import sys
+
+
+def pripravi_imenik(ime_datoteke):
+    '''Če še ne obstaja, pripravi prazen imenik za dano datoteko.'''
+    imenik = os.path.dirname(ime_datoteke)
+    if imenik:
+        os.makedirs(imenik, exist_ok=True)
+
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+def shrani(url, ime_datoteke, vsili_prenos=False):
+    '''Vsebino strani na danem naslovu shrani v datoteko z danim imenom.'''
+    try:
+        print('Shranjujem {}...'.format(url), end='')
+        sys.stdout.flush()
+        if os.path.isfile(ime_datoteke) and not vsili_prenos:
+            print('shranjeno že od prej!')
+            return
+        r = requests.get(url, headers)
+    except requests.exceptions.ConnectionError:
+        print('stran ne obstaja!')
+    else:
+        pripravi_imenik(ime_datoteke)
+        with open(ime_datoteke, 'w', encoding='utf-8') as datoteka:
+            datoteka.write(r.text)
+            print('shranjeno!')
+
+
+def vsebina_datoteke(ime_datoteke):
+    '''Vrne niz z vsebino datoteke z danim imenom.'''
+    with open(ime_datoteke, encoding="utf8") as datoteka:
+        vsebina = datoteka.read()
+    return vsebina
+
+
+def datoteke(imenik):
+    '''Vrne imena vseh datotek v danem imeniku skupaj z imenom imenika.'''
+    return [os.path.join(imenik, datoteka) for datoteka in os.listdir(imenik)]
+
+
+def zapisi_tabelo(slovarji, imena_polj, ime_datoteke):
+    '''Iz seznama slovarjev ustvari CSV datoteko z glavo.'''
+    pripravi_imenik(ime_datoteke)
+    with open(ime_datoteke, 'w', encoding='utf8') as csv_dat:
+        writer = csv.DictWriter(csv_dat, fieldnames=imena_polj)
+        writer.writeheader()
+        for slovar in slovarji:
+            writer.writerow(slovar)
---
+++ zajem_podatkov.py
@@ -0,0 +1,48 @@
+import requests
+import re
+import orodja
+
+server = 'http://www.metacritic.com/browse/albums/score/metascore/all/filtered?view=detailed&%3Bsort=desc&page=0'
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+
+def shrani_html(server):
+    for stran in range(20):
+        r = requests.get('http://www.metacritic.com/browse/albums/score/metascore/all/filtered?view=detailed&%3Bsort=desc&page={}'.format(stran),
+                         headers=headers)
+        ime = 'strani/{}.html'.format(stran)
+        orodja.pripravi_imenik(ime)
+        with open(ime, 'w', encoding='utf-8') as datoteka:
+            datoteka.write(r.text)
+            print('shranjeno!')
+
+regex_albuma = re.compile(r'"product_title"><a.*?>(?P<naslov>.+?)</a><.*?> - (?P<avtor>.*?)</span>'
+                          r'</h3>.*?tive">(?P<ocena_kritikov>\d{2})</s'
+                          r'pan>.*?data">.*?(?P<leto>\d{4})</span>.*?Genre.*?data">.*?(?P<zanr>\D.*?)</s'
+                          r'pan>.*?<span class="data textscore.*?>(?P<ocena_ljudi>.*?)</span>',
+                          flags=re.DOTALL)
+
+imena_polj = ['naslov', 'avtor', 'ocena_kritikov', 'leto', 'zanr', 'ocena_ljudi']
+
+def naredi_csv():
+    sez = []
+    for html in orodja.datoteke('strani/'):
+        counter = 0 #s tem odstranim tezavo pri regexu na koncu vsakega htmlja
+        for album in re.finditer(regex_albuma, orodja.vsebina_datoteke(html)):
+            podatki = album.groupdict()
+            counter += 1
+            podatki['zanr'] = podatki['zanr'].split()
+            podatki['leto'] = int(podatki['leto'])
+            podatki['ocena_kritikov'] = int(podatki['ocena_kritikov'])
+            podatki['ocena_ljudi'] = float(podatki['ocena_ljudi'])
+            if counter >= 70:
+                break
+            sez.append(podatki)
+    orodja.zapisi_tabelo(sez, imena_polj, 'tabela.csv')
+
+#shrani_html(server)
+#naredi_csv()
+
+
+
+
+
